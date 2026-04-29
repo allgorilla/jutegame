c63019b6da5e75c343afcb1f084b2440deb521cf
allgorilla <pow.9093wgtm@docomo.ne.jp>
2026-04-29 09:57:53 +0900

    酒場の入り口のメッセージを実装。

--- scripts/BarScene.gd
+++ scripts/BarScene.gd
@@ -3,6 +3,14 @@ extends Control
 
 @onready var current_gold_label = $StatusPanel/VBox/GoldLine/ValueLabel
 @onready var cost_label = $StatusPanel/VBox/CostLine/ValueLabel
+@onready var message_label = $CanvasLayer/Panel/MessageLabel
+@onready var next_guide = $CanvasLayer/Panel/NextGuide
+
+var messages = [
+	"マリィ：\nここは さかば よ！\nたびのなかまを しょうかい するわ！",
+	"マリィ：\nしょうかいりょうは ５０ゴールド よ！"
+]
+var current_msg_index = 0
 
 func _ready():
 	update_ui()
@@ -12,6 +20,31 @@ func _ready():
 		var anim = changer.get_node("AnimationPlayer")
 		anim.play_backwards("fade")
 		await anim.animation_finished
+	_show_message()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"): # SpaceやEnter
+		_advance_message()
+
+func _show_message():
+	message_label.text = messages[current_msg_index]
+	# 最後のメッセージならガイドを変えるなどの演出
+	next_guide.visible = true
+
+func _advance_message():
+	current_msg_index += 1
+	
+	if current_msg_index < messages.size():
+		_show_message()
+	else:
+		# 全てのメッセージが終わった後の処理
+		print("メッセージ終了。仲間紹介画面へ、または通信開始。")
+		_on_message_finished()
+
+func _on_message_finished():
+	# メッセージ枠を消す、または仲間選択UIを出す
+	$CanvasLayer/Panel.hide()
+	# ここで紹介料のチェックや通信処理（NetworkManagerへの依頼）へ繋げる
 
 func update_ui():
 	# 所持金の表示
