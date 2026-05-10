c236aa03fb9bfa9cb94a8de77efbaa242373d644
allgorilla <pow.9093wgtm@docomo.ne.jp>
2026-05-10 15:55:30 +0900

    敵前列、敵後列の表示に成功。

--- scripts/BattleScene.gd
+++ scripts/BattleScene.gd
@@ -21,46 +21,34 @@ func _ready():
 # 受け取ったデータに基づいてシーンを構築
 func _initialize_battle(data: Dictionary):
 	# --- フロントユニット（Line1, 2）の処理 ---
-	var front_count = BattleContext.front_units.size()
-	for i in range(1, 3): # Line 1, 2
+	_draw_array(BattleContext.front_units,"Line1","Line2")
+	_draw_array(BattleContext.back_units,"Line3","Line4")
+
+func _draw_array(units: Array, line_a:String, line_b:String):
+	var units_count = units.size()
+	for i in range(2):
 		var line_visible = false
-		for j in range(1, 5): # Slot 1, 2, 3, 4
-			var current_index = (i - 1) * 4 + j
-			var is_visible = current_index <= front_count
+		var line_string = line_a if i==0 else line_b
+
+		for j in range(4):
+			var current_index = (i * 4) + j
+			var is_visible = current_index < units_count
 			
-			var slot_node = "EnemyContainer/Line" + str(i) + "/HBoxContainer/UnitSlot" + str(j)
-			var slot = get_node(slot_node)
-			var image = get_node(slot_node + "/Image")
+			var slot = get_node("EnemyContainer/" + line_string + "/HBoxContainer/UnitSlot" + str(j+1))
+			var image = slot.get_node("Image") # slotからの相対パスで書ける
 			slot.visible = is_visible
 			if is_visible:
 				line_visible = true
-				image.texture = _setup_unit_visual(current_index-1)
+				image.texture = _setup_unit_visual(units[current_index])
 
-		get_node("EnemyContainer/Line" + str(i)).visible = line_visible
+		get_node("EnemyContainer/" + line_string).visible = line_visible
 
-	# --- バックユニット（Line3, 4）の処理 ---
-	var back_count = BattleContext.back_units.size()
-	for i in range(3, 5): # Line 3, 4
-		var line_visible = false
-		for j in range(1, 5): # Slot 1, 2, 3, 4
-			# Line3のSlot1を「1番目」としてカウントするための計算
-			var current_index = (i - 3) * 4 + j
-			var is_visible = current_index <= back_count
-			
-			var slot = get_node("EnemyContainer/Line" + str(i) + "/HBoxContainer/UnitSlot" + str(j))
-			slot.visible = is_visible
-			if is_visible: line_visible = true
-			
-		get_node("EnemyContainer/Line" + str(i)).visible = line_visible
-
-
-func _setup_unit_visual(index: int):
-	var unit_data = BattleContext.front_units[index]
+func _setup_unit_visual(unit_data: Dictionary):
 	var unit_id = unit_data.get("my_id")
 	var data = UnitMaster.get_unit_data(unit_id)
-	if data.is_empty(): return
+	if data.is_empty(): return null # テクスチャがない場合はnullを返す
 	
-	# "res://path/to/assets/01.png" のようなパスを組み立てる
+	# パスを組み立ててロード [cite: 4]
 	var path = "res://assets/image/units/" + data["image_id"] + ".png"
 	var texture = load(path)
 	
